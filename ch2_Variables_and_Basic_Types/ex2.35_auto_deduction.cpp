3a5cbfd6e72c03fac02d383f5300449359fe6992
Deokseong247 <deokseongjeong247@gmail.com>
2020-01-11 23:46:03 +0900

    Exercises worked 20/1/11

--- ch2_Variables_and_Basic_Types/ex2.35_auto_deduction.cpp
+++ ch2_Variables_and_Basic_Types/ex2.35_auto_deduction.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+#include <typeinfo>
 
 int main()
 {
     const int i = 42;
-    auto j = i;
-    const auto k = i;
-    auto *p = &i;
-    const auto j2 = i, &k2 = i;
+    auto j = i;  // j in an int
+    const auto k = i;  // k in a const int
+    auto *p = &i;  // p points to an int
+    const auto j2 = i, &k2 = i;  // k2 is a const refernce to int
 
-    std::cout << "i, j, k, p, j2, k2: " << i << j << k << *p << j2 << k2 << std::endl;
+    // std::cout << "i, j, k, p, j2, k2: " << i << j << k << *p << j2 << k2 << std::endl;
+
+    std::cout << "i: " << i << ", " << typeid(i).name() << "\n";
+    std::cout << "j: " << j << ", " << typeid(j).name() << "\n";
+    std::cout << "k: " << k << ", " << typeid(k).name() << "\n";
+    std::cout << "p: " << p << ", " << typeid(p).name() << "\n";
+    std::cout << "j2: " << j2 << ", " << typeid(j2).name() << "\n";
+    std::cout << "k2: " << k2 << ", " << typeid(k2).name() << "\n";
 
     return 0;
 }
